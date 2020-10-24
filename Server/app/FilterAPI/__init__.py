24dfbc9bc83d9af1285f16629a4eb0671a2382e7
kmstage <kave.moradian@gmail.com>
2020-10-24 13:48:55 +0330

    sqlite as a test database

--- Server/app/FilterAPI/__init__.py
+++ Server/app/FilterAPI/__init__.py
@@ -5,7 +5,11 @@ from FilterAPI.Config import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = app_secret_key
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{db_username}:{db_password}@{db_server}/{db_name}?charset=utf8mb4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Database.db'
+'''
+--> use postgresql for deploy: 
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_username}:{db_password}@{db_server}/{db_name}?charset=utf8mb4'
+'''
 db = SQLAlchemy(app)
 
 '''
